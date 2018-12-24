1133b63f50e75e385497c6ba26eb6c40741ed3b9
your name here <theo@dali.dartmouth.edu>
2018-12-24 13:23:18 -0600

    Add express routes

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
-
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
