6e495f7bd0e4efb7255129db5a70d4c574c0bc8a
your name here <theo@dali.dartmouth.edu>
2018-12-25 14:55:46 -0600

    setup User model

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const User = require('.models/User');
 
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
@@ -16,6 +20,17 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-app.get("/", (req, res) => res.send("Testing"));
+
+app.get("/", (req, res) => {
+  const user = new User({
+    handle: 'theo', 
+    email: 'test@test', 
+    password: 'password'
+  })
+  user.save()
+  res.send('hello wurld');
+});
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,9 +4,18 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
 
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
+
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({
+    id: req.user.id,
+    handle: req.user.handle,
+    email: req.user.email
+  });
+})
 
 router.post("/register", (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
