6f05a81d998e2509537a69213d18509318193387
your name here <theo@dali.dartmouth.edu>
2018-12-31 11:30:20 -0800

    Fix bug and get user auth fully working

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -39,15 +39,15 @@ export const signup = user => dispatch => (
 
 // Upon login, set the session token and dispatch the current user. Dispatch errors on failure.
 export const login = user => dispatch => (
-  APIUtil.login(user).then(res => {
-    const { token } = res.data;
-    localStorage.setItem('jwtToken', token);
-    APIUtil.setAuthToken(token);
-    const decoded = jwt_decode(token);
-    dispatch(receiveCurrentUser(decoded))
-  })
+    APIUtil.login(user).then(res => {
+        const { token } = res.data;
+        localStorage.setItem('jwtToken', token);
+        APIUtil.setAuthToken(token);
+        const decoded = jwt_decode(token);
+        dispatch(receiveCurrentUser(decoded))
+    })
     .catch(err => {
-      dispatch(receiveErrors(err.response.data));
+        dispatch(receiveErrors(err.response.data));
     })
 )
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -5,95 +5,94 @@ const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
+
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 
-router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
-  res.json({
-    id: req.user.id,
-    handle: req.user.handle,
-    email: req.user.email
-  });
-})
+router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
-router.post("/register", (req, res) => {
-  
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.json({
+      id: req.user.id,
+      handle: req.user.handle,
+      email: req.user.email
+    });
+  })
+
+router.post('/register', (req, res) => {
   const { errors, isValid } = validateRegisterInput(req.body);
 
   if (!isValid) {
     return res.status(400).json(errors);
   }
 
-  User.findOne({ handle: req.body.handle }).then(user => {
+    // Check to make sure nobody has already registered with a duplicate email
     User.findOne({ email: req.body.email })
       .then(user => {
-        
         if (user) {
-          // Use the validations to send the error
-          errors.email = 'Email already exists';
-          return res.status(400).json(errors);
+          // Throw a 400 error if the email address already exists
+          return res.status(400).json({email: "A user has already registered with this address"})
         } else {
+          // Otherwise create a new user
           const newUser = new User({
             handle: req.body.handle,
             email: req.body.email,
-            password: req.body.password, 
+            password: req.body.password
           })
+
           bcrypt.genSalt(10, (err, salt) => {
             bcrypt.hash(newUser.password, salt, (err, hash) => {
               if (err) throw err;
               newUser.password = hash;
-              newUser
-                .save()
-                .then(user => {
-                  const payload = { id: user.id, handle: user.handle };
-
-                  jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
-                    res.json({
-                      success: true,
-                      token: "Bearer " + token
-                    });
-                  });
-                })
+              newUser.save()
+                .then(user => res.json(user))
                 .catch(err => console.log(err));
-            });
-          });
+            })
+          })
         }
       })
+  })
+
+
+  router.post('/login', (req, res) => {
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    console.log(errors);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
     }
-  );
-});
 
-router.post("/login", (req, res) => {
-  const { errors, isValid } = validateLoginInput(req.body);
-
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  const email = req.body.email;
-  const password = req.body.password;
-
-  User.findOne({ email })
-    .then(user => {
-      if (!user) {
-        // Use the validations to send the error
-        errors.email = 'User not found';
-        return res.status(404).json(errors);
-      }
-
-      bcrypt.compare(password, user.password)
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    User.findOne({email})
+      .then(user => {
+        if (!user) {
+          return res.status(404).json({email: 'This user does not exist'});
+        }
+  
+        bcrypt.compare(password, user.password)
         .then(isMatch => {
-          if (isMatch) {
-            res.json({ msg: 'Success' });
-          } else {
-            // And here:
-            errors.password = 'Incorrect password'
-            return res.status(400).json(errors);
-          }
-        })
-    })
-});
+            if (isMatch) {
+            const payload = {id: user.id, name: user.name};
 
-router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+            jwt.sign(
+                payload,
+                keys.secretOrKey,
+                // Tell the key to expire in one hour
+                {expiresIn: 3600},
+                (err, token) => {
+                res.json({
+                    success: true,
+                    token: 'Bearer ' + token
+                });
+              });
+            } else {
+                return res.status(400).json({password: 'Incorrect password'});
+            }
+        })
+      })
+  })
 
 module.exports = router;
